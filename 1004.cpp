7e1193fa4463021bc01f448a95ab92289a13efab
c3030991 <88984126@qq.com>
2019-04-01 17:48:26 +0800

    update 1004

--- 1004.cpp
+++ 1004.cpp
@@ -1,1 +1,18 @@
-12321
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	double sum = 0, money;
+	int i;
+	for (i = 1; i <= 12; i++)
+	{
+		cin >> money;
+		sum += money;
+	}
+	cout << "$" << sum / 12 << endl;
+	system("pause");
+	return 0;
+}
